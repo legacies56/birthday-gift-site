6339cc74035263123f2946981d9263faba62f465
legacies56 <mohamedanwer3061@gmail.com>
2025-06-26 15:41:34 +0300

    doggy dog

---
+++ index.js
@@ -0,0 +1,13 @@
+let message = ""
+let messageEL = document.getElementById("message");
+let giftImages = document.getElementsByClassName("gift-img");
+for (let i = 0; i < giftImages.length; i++) {
+    giftImages[i].onmouseout = function() {
+        alert("Bir bybi ye metnak");
+    };
+}
+
+
+window.onload = function() {
+    alert("Welcome DOG!");
+}
